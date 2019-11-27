46cea7905c03be69110b76d56da378bbf172761d
Marina Gjorgjievski <marina.gjorgjievski@outlook.com>
2019-11-27 17:11:10 +0100

    Updates

---
+++ precache-manifest.dc2b702e7d521c85c37c0d5c368c2733.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b0e158f8d111ab0fc8e30bda3798a27",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e97070d393ad87044474",
+    "url": "/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "3e1008584e3eaa9df8cb",
+    "url": "/static/css/main.22e8b499.chunk.css"
+  },
+  {
+    "revision": "e97070d393ad87044474",
+    "url": "/static/js/2.814c5b91.chunk.js"
+  },
+  {
+    "revision": "3e1008584e3eaa9df8cb",
+    "url": "/static/js/main.cb82e550.chunk.js"
+  },
+  {
+    "revision": "9bf820bd41e6d656f8db",
+    "url": "/static/js/runtime-main.f59df13c.js"
+  },
+  {
+    "revision": "577d173b5cbb6b384fae8055c727c84e",
+    "url": "/static/media/Ironworks.577d173b.woff"
+  }
+]);
